27a0057161114e67b7f71200541b9c122786962f
Lucas Oliveira <lucascs.oliveira@gmail.com>
2014-12-17 01:06:19 -0200

    Add View + Controller
    Just added the empty classes App.java (View) and AplDocumentClassification.java;

---
+++ src/main/java/so/coutinho/lucas/guesswork/view/App.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package so.coutinho.lucas.guesswork.view;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ *
+ * @author Lucas
+ */
+public class App {
+
+    public static void main(String[] args) throws IOException {
+        //TODO: Create main method
+    }
+
+}
