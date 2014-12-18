d7c2629dfad447cbff9b6e7a12b3e97d8436d025
unknown <lucas.oliveira@PDCASEVIX036-PC.pdcasevix.com.br>
2014-12-18 16:02:27 -0200

    Add layer controller
    Add AplDocumentClassification in controller package;

--- src/main/java/so/coutinho/lucas/guesswork/controller/AplDocumentClassification.java
+++ src/main/java/so/coutinho/lucas/guesswork/controller/AplDocumentClassification.java
@@ -5,10 +5,42 @@
  */
 package so.coutinho.lucas.guesswork.controller;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import so.coutinho.lucas.guesswork.model.Document;
+import so.coutinho.lucas.guesswork.model.DocumentClassifier;
+import so.coutinho.lucas.guesswork.model.KNearestNeighbor;
+
 /**
  *
  * @author Lucas
  */
 public class AplDocumentClassification {
-    //TODO: Create AplDocumentClassification
+
+    private final KNearestNeighbor kNearestNeighbor;
+
+    public AplDocumentClassification(List<Document> list) {
+        kNearestNeighbor = new KNearestNeighbor(list);
+    }
+
+    public Iterator<Document> getKNearestDocuments(Integer K, Document document) {
+        return kNearestNeighbor.getKNeighbors(document, K);
+    }
+
+    public void classify(Document document) {
+        Iterator<Document> iteratorNeighbors = kNearestNeighbor.getAllNeighbors(document);
+        List<Document> listNeighbors = new ArrayList<>();
+
+        while (iteratorNeighbors.hasNext()) {
+            listNeighbors.add(iteratorNeighbors.next());
+        }
+
+        DocumentClassifier.classify(document, listNeighbors);
+    }
+
+    public List<Document> getDocumentsByCategory(String category) {
+        return kNearestNeighbor.getDocumentsByCategory(category);
+    }
+
 }
